15a28cc88d55a4a5223d4c5fb6e613fe853071d5
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-10-11 10:51:45 -0300

    feat: added auth verification on pages

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,3 +1,11 @@
+import { useContext } from "react";
+
+import { UserContext } from "../../contexts/userContext";
+import { useProtectedRoutes } from "../../hooks/useProtectedRoutes";
+
 export const Dashboard = () => {
+  const { isLogged } = useContext(UserContext);
+  useProtectedRoutes(isLogged, true);
+
   return <div>Home</div>;
 };
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,3 +1,28 @@
+import { useContext } from "react";
+
+import { ContentBoxDiv } from "../../components/ContentBoxDiv";
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { HomeMain } from "../../components/HomeMain";
+import { ModalLogin } from "../../components/Modals/ModalLogin";
+import { ModalRegister } from "../../components/Modals/ModalRegister";
+
+import { UserContext } from "../../contexts/userContext";
+import { useProtectedRoutes } from "../../hooks/useProtectedRoutes";
+
 export const HomePage = () => {
-  return <div className="">Home</div>;
+  const { isLogged } = useContext(UserContext);
+  useProtectedRoutes(isLogged, false);
+
+  return (
+    <div className="bg-gray-300 w-full  ">
+      <div className="bg-home bg-cover h-screen w-full absolute opacity-10 shadow-[0_100px_100px_#070516]" />
+      <Header />
+      <HomeMain />
+      <ContentBoxDiv />
+      <Footer />
+      <ModalLogin />
+      <ModalRegister />
+    </div>
+  );
 };

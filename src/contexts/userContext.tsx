eb6c4d171ba29a4770ad1b84a932daa842d08277
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-10-11 10:49:06 -0300

    feat: create userContext functions

--- src/contexts/userContext.tsx
+++ src/contexts/userContext.tsx
@@ -1,4 +1,7 @@
 import { createContext, useState } from "react";
+import { Api } from "../services/Api";
+import { CustomToast } from "../components/Toast";
+import { useNavigate } from "react-router-dom";
 
 interface IUserContextProps {
   children: React.ReactNode;
@@ -6,33 +9,104 @@ interface IUserContextProps {
 
 interface IUserContext {
   isLoading: boolean;
+  isRegisterSuccess: boolean;
+  isLogged: boolean;
+
+  Register: (data: IUserDataRegister) => void;
+  Login: (data: IUserDataLogin) => void;
+  Logout: () => void;
+  setIsRegisterSuccess: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-//   interface IUserDataRegister {
-//     name: string;
-//     password: string;
-//     confirmPassword: string;
-//   }
+interface IUserDataRegister {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
-//   interface IUserDataLogin {
-//     email: string;
-//     password: string;
-//   }
+interface IUserDataLogin {
+  email: string;
+  password: string;
+}
 
-//   interface IUserApiLoginResp {
-//     accessToken: string;
-//   }
+interface IUserApiLoginResp {
+  accessToken: string;
+}
 
 export const UserContext = createContext<IUserContext>({} as IUserContext);
 
 export const UserProvider = ({ children }: IUserContextProps) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isLogged, setIsLogged] = useState(
+    !!localStorage.getItem("@TODO:TOKEN")
+  );
   const [isLoading, setIsLoading] = useState(false);
+  const [isRegisterSuccess, setIsRegisterSuccess] = useState(false);
+
+  const navigate = useNavigate();
+  const { toastify } = CustomToast();
+
+  const Register = async (data: IUserDataRegister) => {
+    try {
+      await Api.post("register", {
+        email: data.email,
+        password: data.password,
+      });
+      setIsRegisterSuccess(true);
+
+      toastify({
+        description: "Usuário cadastrado com sucesso!",
+        status: "success",
+      });
+    } catch (error) {
+      toastify({
+        description: "Ops, algo deu errado tente novamente!",
+        status: "error",
+      });
+      setIsRegisterSuccess(false);
+      return error;
+    }
+  };
+
+  const Login = async (data: IUserDataLogin) => {
+    try {
+      setIsLoading(true);
+      const res = await Api.post<IUserApiLoginResp>("login", data);
+      localStorage.clear();
+      localStorage.setItem("@TODO:TOKEN", res.data.accessToken);
+      setIsLogged(true);
+
+      navigate("/dashboard");
+      toastify({
+        description: "Login realizado com sucesso!",
+        status: "success",
+      });
+    } catch (error) {
+      toastify({
+        description: "E-mail ou senha inválido, tente novamente!",
+        status: "error",
+      });
+      return error;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const Logout = () => {
+    setIsLogged(false);
+    localStorage.removeItem("@TODO:TOKEN");
+    navigate("/");
+  };
 
   return (
     <UserContext.Provider
       value={{
         isLoading,
+        isRegisterSuccess,
+        isLogged,
+        setIsRegisterSuccess,
+        Register,
+        Login,
+        Logout,
       }}
     >
       {children}

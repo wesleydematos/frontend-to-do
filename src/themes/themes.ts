ddf6a61cc1d0577b5b7060555db7af20f95e8dc3
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-10-11 09:09:09 -0300

    feat/style: create theme

---
+++ src/themes/themes.ts
@@ -0,0 +1,16 @@
+export const Colors = {
+  transparent: "transparent",
+  black: "#000000",
+  white: "#ffffff",
+  black_rgba: "rgba(0, 0, 0, 0.54)",
+  green100: "#61FFAA",
+  green200: "#08490e",
+  green300: "#38F892",
+  yellow100: "#FACD35",
+  error100: "#E64980",
+  gray100: "#C7C7C7",
+  gray200: "#353149",
+  gray300: "#221E34",
+  gray400: "#100D1E",
+  gray500: "#1D1A2C",
+};
